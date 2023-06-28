449e4b06e0c0f436b12f551cf47e76232b9253ea
tim <tim@elec.ac.nz>
2023-06-28 12:08:44 +1200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # neoden-yy1-kicad
-Post-processor to allow Kicad centroid files to work on the Neoden YY1 pick and place machine
+
+Post-processor to allow Kicad centroid files to work on the Neoden YY1 pick and place machine. This package contains a script that reads CPL (centroid position) files and modifies their contents to be readable by the [Neoden YY1](https://www.neodensmt.com/pick-and-place-machine/neoden-yy1-pick-and-place-machine.html) small pick and place machine.
+
+## Usage
+
+    neoden_kicad --pos my_pos.csv --out neoden_pos.csv
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name='neoden_kicad',
+    version='1.0.0a1',
+    description='Converter for Neoden Pick and Place machine',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url='http://github.com/elec-otago/neoden-yy1-kicad',
+    author='Tim Molteno, Phill Brown',
+    author_email='tim@elec.ac.nz',
+    license='GPLv3',
+    install_requires=[],
+    packages=['neoden_kicad'],
+    scripts=['bin/neoden_kicad'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        'Programming Language :: Python :: 3',
+        "Intended Audience :: End Users/Desktop",  ])
